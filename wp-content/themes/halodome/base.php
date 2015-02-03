3dbea4f60aac9d85b94a884c6e513c2b26bcb7bd
alvarpoon <michiel@sedgwick-richardson.com>
2015-02-03 22:48:06 +0800

    mainpage

--- wp-content/themes/halodome/base.php
+++ wp-content/themes/halodome/base.php
@@ -17,6 +17,6 @@
       </main>
 
   <?php get_template_part('templates/footer'); ?>
-
+  <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/halodome/templates/head.php
+++ wp-content/themes/halodome/templates/head.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
-
   <?php wp_head(); ?>
   <!--[if lt IE 9]>
     <script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
