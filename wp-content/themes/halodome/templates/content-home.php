81e4bcb3944767e080ecbbebd3adc24d1e3083c0
alvarpoon <michiel@sedgwick-richardson.com>
2015-02-07 00:28:16 +0800

    0207 0:27

--- wp-content/themes/halodome/templates/content-home.php
+++ wp-content/themes/halodome/templates/content-home.php
@@ -1,43 +1,47 @@
-<section class="full-width-image">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-1.jpg" alt="" />
-	<div>
-		<h1>HALODOME</h1>
-		<h3>Spectacular Space</h3>
-	</div>
-	<div class="image-caption">
-		<p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
-	</div>
+<!--<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-1.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
+  <div>
+    <h1>HALODOME</h1>
+    <h3>Spectacular Space</h3>
+  </div>
+  <div class="image-caption">
+    <p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
+  </div>
+</section>-->
+<section class="">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-1.jpg" class="bg" />
+  <div>
+    <h1>HALODOME</h1>
+    <h3>Spectacular Space</h3>
+  </div>
+  <div class="image-caption">
+    <p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
+  </div>
 </section>
-<section class="full-width-image">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-2.jpg" alt="" />
-	<div class="image-caption">
-		<p>Domes are ancient, from igloos to The Pantheon the experience inside them is visceral and wonderful with a e nveloping sense of calm created by the arching walls that become roof and then walls again in a single sweep.</p>
-	</div>
+<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-2.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
+  <div class="image-caption">
+    <p>Domes are ancient, from igloos to The Pantheon the experience inside them is visceral and wonderful with a e nveloping sense of calm created by the arching walls that become roof and then walls again in a single sweep.</p>
+  </div>
 </section>
-<section class="full-width-image">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-3.jpg" alt="" />
-	<div class="image-caption image-caption-right">
-		<p>Built to German Passivehaus specification, the highest standard in the world for energy consumption, the concept uses seventy fully prefabricated and sustainably sourced timber panels bolted together to form a single dome space 13 meters x 6.5 meters high.</p>
-	</div>
+<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-3.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
+  <div class="image-caption">
+    <p>Built to German Passivehaus specification, the highest standard in the world for energy consumption, the concept uses seventy fully prefabricated and sustainably sourced timber panels bolted together to form a single dome space 13 meters x 6.5 meters high.</p>
+  </div>
 </section>
-<section class="full-width-image" id="halodome-numbers">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-4.jpg" alt="" />
+<section  style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-4.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active" id="halodome-numbers">
 	<h2>Numbers in Halodome</h2>
 	<div class="">
 	</div>
 </section>
-<section class="full-width-image">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-5.jpg" alt="" />
-	<div class="image-caption image-caption-right">
-		<p>The space is completely flexible an can be offered, as in the example shown as a 160m<sup>2</sup> of usable space on two levels with a half size mezzanine floor and central spiral staircase.</p>
-	</div>
+<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-5.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
+  <div class="image-caption">
+    <p>The space is completely flexible an can be offered, as in the example shown as a 160m<sup>2</sup> of usable space on two levels with a half size mezzanine floor and central spiral staircase.</p>
+  </div>
 </section>
-<section class="full-width-image" id="halodome-floorplan">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-6.jpg" alt="" />
+<section class="full-width-image" id="halodome-floorplan" style="text-align:center;">
 	<h2>Halodome Floorplan</h2>
+	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/floor-plan.jpg" alt="" style="margin:0 auto;"/>
 </section>
-<section class="full-width-image" id="halodome-floorplan">
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-7.jpg" alt="" />
+<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-6.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active">
 </section>
 <section class="full-width-video">
 
