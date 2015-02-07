2c1398d277c89ae9c3c93ec5d89ce9c1516bfc81
alvarpoon <michiel@sedgwick-richardson.com>
2015-02-08 01:55:21 +0800

    0208 1:55

--- wp-content/themes/halodome/templates/content-home.php
+++ wp-content/themes/halodome/templates/content-home.php
@@ -1,48 +1,53 @@
-<!--<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-1.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
-  <div>
-    <h1>HALODOME</h1>
-    <h3>Spectacular Space</h3>
-  </div>
-  <div class="image-caption">
-    <p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
-  </div>
-</section>-->
-<section class="">
+<section class="full-width-container">
   <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-1.jpg" class="bg" />
-  <div>
-    <h1>HALODOME</h1>
-    <h3>Spectacular Space</h3>
-  </div>
-  <div class="image-caption">
-    <p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
-  </div>
-</section>
-<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-2.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
-  <div class="image-caption">
-    <p>Domes are ancient, from igloos to The Pantheon the experience inside them is visceral and wonderful with a e nveloping sense of calm created by the arching walls that become roof and then walls again in a single sweep.</p>
-  </div>
-</section>
-<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-3.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
-  <div class="image-caption">
-    <p>Built to German Passivehaus specification, the highest standard in the world for energy consumption, the concept uses seventy fully prefabricated and sustainably sourced timber panels bolted together to form a single dome space 13 meters x 6.5 meters high.</p>
-  </div>
-</section>
-<section  style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-4.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active" id="halodome-numbers">
-	<h2>Numbers in Halodome</h2>
-	<div class="">
+  <? get_template_part('templates/header-top-navbar');?>
+  <div class="overlayContent">
+	  <div style="border: 1px solid #fff; margin:16% auto 0; min-width: auto; width: 30%;">
+		<h1>HALODOME</h1>
+		<h3>Spectacular Space</h3>
+	  </div>
+	  <div class="image-caption">
+		<p>HALODOME is a prefabricated multi-use timber structure conceived by Tim Oulton, founder of the Halo group, for potential export to anywhere in the world, "a house, a workshop, a studio, a showroom, what ever you want…” as Tim says.</p>
+	  </div>
 	</div>
 </section>
-<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-5.jpg); height:511px; min-height:auto;" class="cd-fixed-bg slide cd-bg-1 ss-active"> 
-  <div class="image-caption">
-    <p>The space is completely flexible an can be offered, as in the example shown as a 160m<sup>2</sup> of usable space on two levels with a half size mezzanine floor and central spiral staircase.</p>
-  </div>
+<section class="full-width-container">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-2.jpg" class="bg" />
+  <div class="overlayContent">
+	  <div class="image-caption">
+		<p>Domes are ancient, from igloos to The Pantheon the experience inside them is visceral and wonderful with a e nveloping sense of calm created by the arching walls that become roof and then walls again in a single sweep.</p>
+	  </div>
+	</div>
 </section>
-<section class="full-width-image" id="halodome-floorplan" style="text-align:center;">
-	<h2>Halodome Floorplan</h2>
-	<img class="img-responsive" src="<?=get_stylesheet_directory_uri()?>/assets/img/floor-plan.jpg" alt="" style="margin:0 auto;"/>
+<section class="full-width-container half-height">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-3.jpg" class="bg" />
+  <div class="rightContent">
+	  <div class="image-caption">
+		<p>Built to German Passivehaus specification, the highest standard in the world for energy consumption, the concept uses seventy fully prefabricated and sustainably sourced timber panels bolted together to form a single dome space 13 meters x 6.5 meters high.</p>
+	  </div>
+	</div>
 </section>
-<section style="background-image: url(<?=get_stylesheet_directory_uri()?>/assets/img/main-img-6.jpg);" class="cd-fixed-bg slide cd-bg-1 ss-active">
+<section class="full-width-container half-height">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-4.jpg" class="bg" />
+  <div class="overlayContent" style="background:url(<?=get_stylesheet_directory_uri()?>/assets/img/bg_alpha.png) repeat;">
+  		<h2>Numbers in Halodome</h2>
+	  <div class="image-caption">
+		
+	  </div>
+	</div>
 </section>
-<section class="full-width-video">
-
+<section class="full-width-container half-height">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-5.jpg" class="bg" />
+  <div class="leftContent">
+	  <div class="image-caption">
+		<p>The space is completely flexible an can be offered, as in the example shown as a 160m<sup>2</sup> of usable space on two levels with a half size mezzanine floor and central spiral staircase.</p>
+	  </div>
+	</div>
+</section> 		
+<section class="full-width-container" id="halodome-floorplan">
+  <h2>Halodome Floorplan</h2>
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/floor-plan.jpg" />
+</section>
+<section class="full-width-container">
+  <img src="<?=get_stylesheet_directory_uri()?>/assets/img/main-img-6.jpg" class="bg" />
 </section>
--- wp-content/themes/halodome/templates/header-top-navbar.php
+++ wp-content/themes/halodome/templates/header-top-navbar.php
@@ -1,5 +1,5 @@
-<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container">
+<header class="banner navbar navbar-default navbar-absolute-top" role="banner">
+  <!--<div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
@@ -8,12 +8,13 @@
         <span class="icon-bar"></span>
       </button>
       <a href="javascript:;" class="menu-label hidden-xs hidden-sm hidden-md hidden-lg" data-toggle="collapse" data-target=".navbar-collapse">menu</a>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><!-- <img src="<?//=get_stylesheet_directory_uri()?>/assets/img/logo-top.png"> --></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"></a>
     </div>
-  </div>
+  </div>-->
   <div class="nav-container">
+  	<div style="float:left; background:url(<?=get_stylesheet_directory_uri()?>/assets/img/icon.png)  no-repeat; width:90px; height:60px; margin:20px 30px;"></div>
     <nav class="collapse navbar-collapse main-menu" role="navigation">
-        <?php
+		<?php
             //Main menu
             if (has_nav_menu('primary_navigation')) :
               wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 0));
