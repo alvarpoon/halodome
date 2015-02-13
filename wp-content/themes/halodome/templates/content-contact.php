463e5dc9674fbc3dcb530acaf5f845fc92b045f3
Ron Chan <ksc.ron@gmail.com>
2015-02-13 13:17:11 +0800

    ron update 0213

--- wp-content/themes/halodome/templates/content-contact.php
+++ wp-content/themes/halodome/templates/content-contact.php
@@ -1,2 +1,53 @@
-<?php the_content(); ?>
-<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php //the_content(); ?>
+<?php //wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+
+<?php //the_content(); ?>
+<?php //wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+
+<section class="full-width-container">
+  <img id="mainImg1" class="bg" 
+  	data-bttrlazyloading-animation="fadeIn"
+  	data-bttrlazyloading-xs-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-1.jpg" 
+  	data-bttrlazyloading-sm-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-1.jpg"
+    data-bttrlazyloading-md-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-1.jpg"
+    data-bttrlazyloading-lg-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-1.jpg" />
+  <div class="pageHeadingAbsolute hidden-sm hidden-md hidden-lg">
+	<h1>HALODOME</h1>
+	<h3>Spectacular Space</h3>
+  </div>
+  <div class="overlayContent">
+	  <div class="pageHeading hidden-xs">
+		<h1>HALODOME</h1>
+		<h3>Spectacular Space</h3>
+	  </div>
+  </div>
+</section>
+<section class="full-width-container half-height">
+  <img id="mainImg2" class="bg" 
+  	data-bttrlazyloading-animation="fadeIn"
+  	data-bttrlazyloading-xs-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-2.jpg" 
+  	data-bttrlazyloading-sm-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-2.jpg"
+    data-bttrlazyloading-md-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-2.jpg"
+    data-bttrlazyloading-lg-src="<?=get_stylesheet_directory_uri()?>/assets/img/contact/contact-img-2.jpg" />
+  <div class="overlayContent fullOverlay">
+	<div class="col-sm-7 contactContainer">
+    	<div class="title">
+        	Halodome
+        </div>
+        <div class="contactInfo">
+        	<div>
+	        	Craig Wilson <br />
+    	        T. +44 7881587231<br />
+        	    E. info@halodome.net
+            </div>
+            <div>
+            	Halodome Ltd<br />
+                17F 2 Heung Yip Street<br />
+                Wong Chuk Hang<br />
+                Hong Kong<br />
+                WWW.halodome.net
+            </div>
+        </div>
+    </div>
+  </div>
+</section>
