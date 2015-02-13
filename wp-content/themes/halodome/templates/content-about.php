d43fe0d208a5c48ea05f9e949e1376ccffefeb37
Ron Chan <ksc.ron@gmail.com>
2015-02-13 12:48:19 +0800

    ron update 0213

--- wp-content/themes/halodome/templates/content-about.php
+++ wp-content/themes/halodome/templates/content-about.php
@@ -22,3 +22,58 @@
 	  </div>
 	</div>
 </section>
+<section class="full-width-container ">
+  <img id="mainImg2" class="bg" 
+  	data-bttrlazyloading-animation="fadeIn"
+  	data-bttrlazyloading-xs-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-2.jpg" 
+  	data-bttrlazyloading-sm-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-2.jpg"
+    data-bttrlazyloading-md-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-2.jpg"
+    data-bttrlazyloading-lg-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-2.jpg" />
+  <div class="overlayContent fullOverlay">
+	<h2>ABOUT HALODOME</h2>  
+    <div class="col-sm-10 col-lg-8 aboutParagraph">
+    	<p>Halodome is a pre-fabricated timber building designed to ship anywhere in the world for a multitude of uses “a house, a studio, a showroom a workshop, whatever you want” as founder Tim Oulton says.</p>
+        <p>The design uses a high level of airtightness and insulation to achieve ultra low energy consumption. Triple-glazed doors and windows, of the highest available European quality, are preinstalled into the finished panels and can be positioned in various configurations to suit the location and orientation of the building. A highly efficient ventilation and energy recovery system constantly replenishes fresh air ensuring the temperature inside remains constant and comfortable all year round.</p>
+        <p>The 13 meter diameter column free dome is made up of 70 individual pre-finished panels transported to site in standard shipping containers. The panels use fully certified PEFC timber framing and reclaimed timber linings that bolt together on a level concrete ground slab preinstalled by the customer. Exterior battening and roofing shingles are then fitted, followed by taping of the airtight interior membranes and installation of the timber linings to complete the basic dome shell. A standard Halodome provides approximately 160m2 of floor area if the standard design half floor mezzanine and spiral staircase are installed.</p>
+        <p>Pricing is dependent on specific design requirements, however the basic kit includes all the components necessary to construct a free standing weathertight dome shell with interior reclaimed timber linings. The 70 factory produced panels that form the structure incorporate, insulation, air tight membranes and factory fitted high performance windows and doors. Canadian red cedar roof shingles and treated battens are supplied for cladding onsite along with all stainless steel door and window flashings and a stainless steel dome cap. The structure is accredited by UK structural engineers to UK standards and can also be supplied to meet Passivhaus standard, the highest in the world for energy conservation.</p>
+    </div>
+    <div class="col-sm-10 col-lg-8 aboutList">
+        <div class="col-sm-6 floatLeft">
+            <div class="title">As part of the standard package Halodome Ltd will also supply:</div>
+            <ul>
+            	<li>Thermal modelling of your preferred internal layout, and the required orientation and glazing design to achieve Passivhaus standard.</li>
+                <li>Full triple glazing package comprising - 2 doors with side lights and 4 opening windows for the ground floor and 9 opening roof light windows.</li>
+                <li>A minimum specification for the foundation slab & services, local engineers will be required to check and adjust the design to suit local site conditions.</li>
+                <li>Onsite technical support during assembly of the outer shell, based on 1 person for 5 days.</li>
+            </ul>
+            
+            <div class="title">As an optional extra and not included in our standard price we can provide:</div>
+            <ul>
+            	<li>Delivery in containers to the nearest port or your site.</li>
+                <li>Our standard design of internal mezzanine and spiral staircase.</li>
+                <li>Additional windows or doors, the will affect passivhaus compliance in most locations.</li>
+                <li>Mechanical ventilation and heat recovery units to suit the local climate.</li>
+                <li>Ongoing support during full Passivhaus certification.</li>
+                <li>Customer supplied interior linings and exterior roof claddings.</li>
+                <li>Additional on-site technical support during assembly.</li>
+                <li>Furnishings selected from the Halo range.</li>
+            </ul>
+        </div>
+        <div class="col-sm-6 floatRight">
+        	<div class="title">The Halodome is a factory produced building designed to be assembled and finished by the customer. Under this arrangement the customer is responsible for providing the following:</div>
+			<ul>
+            	<li>All and any reuqired architectural services and advice.</li>
+                <li>Planning consent and all local regulatory compliance relating to the building including fire and safety etc.</li>
+            	<li>All design related to the internal layout, taking into account Passivhaus specifications, if required.</li>
+                <li>All design and installation of the foundations, floor slab and all services. Halodome will provide specifications for the minimum standard of required foundation. </li>
+                <li>All internal ground floor finishes, wall partitions, kitchens, bathrooms, white goods etc.</li>
+                <li>All labour and handing equipment for unloading of the containers and assembly of the building including taping of air tight internal membranes and installation of the timber linings.</li>
+            	<li>All electrical & plumbing services.</li>
+            	<li>Fire retardant treatment of the internal linings or mezzanine timbers to meet local reuqirements.</li>
+            	<li>Site access.</li>
+            	<li>All site insurance etc.</li>
+            </ul>
+        </div>
+      </div>
+  </div>
+</section>
