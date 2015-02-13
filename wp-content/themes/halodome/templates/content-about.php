2ae75a66bc36c82f5274032b3d8f07094b15ca34
Ron Chan <ksc.ron@gmail.com>
2015-02-13 10:52:06 +0800

    ron update 0213

--- wp-content/themes/halodome/templates/content-about.php
+++ wp-content/themes/halodome/templates/content-about.php
@@ -1,2 +1,24 @@
-<?php the_content(); ?>
-<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php //the_content(); ?>
+<?php //wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+
+<section class="full-width-container">
+  <img id="mainImg1" class="bg" 
+  	data-bttrlazyloading-animation="fadeIn"
+  	data-bttrlazyloading-xs-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-1.jpg" 
+  	data-bttrlazyloading-sm-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-1.jpg"
+    data-bttrlazyloading-md-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-1.jpg"
+    data-bttrlazyloading-lg-src="<?=get_stylesheet_directory_uri()?>/assets/img/about/about-img-1.jpg" />
+  <div class="pageHeadingAbsolute hidden-sm hidden-md hidden-lg">
+	<h1>HALODOME</h1>
+	<h3>Spectacular Space</h3>
+  </div>
+  <div class="overlayContent">
+	  <div class="pageHeading hidden-xs">
+		<h1>HALODOME</h1>
+		<h3>Spectacular Space</h3>
+	  </div>
+	  <div class="image-caption">
+		<p>Built to German Passivehaus specification, the highest standard in the world for energy consumption, the concept uses eighty fully prefabricated and sustainably sourced timber panels bolted together to form a single dome space 13 meters x 6.5 meters high.</p>
+	  </div>
+	</div>
+</section>
